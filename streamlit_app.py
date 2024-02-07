3a22792a8a0c8087b6c17ede5f8559608d91a95b
Gabriel do Nascimento <54758393+gabuxt@users.noreply.github.com>
2024-02-06 22:15:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,6 @@ def ui():
     Page_cliente = st.sidebar.selectbox(
     'Menu', ['Menu Principal','Clusterização', 'Inadimplência'], 0)
 
-    st.set_page_config("Streamlit Components Hub", "🏦", layout="wide")
     icon("🏦")
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
 
