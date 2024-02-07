9553fd1204fda25241c03746fd78409d1eccbba3
Gabriel do Nascimento <54758393+gabuxt@users.noreply.github.com>
2024-02-06 21:40:47 -0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ def dados_inadimplencia(body):
         return None
 
 def ui():
+    st.set_page_config("Streamlit Components Hub", "💰", layout="wide")
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
 
     st.title('FINTECH QUANTUMFINANCE')
