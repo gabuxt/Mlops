41427d40d341d28b9d67ed3680ccee5ac7bfd920
Gabriel do Nascimento <54758393+gabuxt@users.noreply.github.com>
2024-02-06 22:41:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,14 +18,14 @@ def icon(emoji: str):
 
 def ui():
     Page_cliente = st.sidebar.selectbox(
-    'Menu', ['Inicio','Avaliação de Crédito', 'Inadimplência'], 0)
+    'Menu', ['Inicio','Avaliação de Crédito', 'Propensão de Inadimplência'], 0)
 
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
 
     if Page_cliente == 'Avaliação de Crédito':
         page_clust.customer_clustering()
 
-    if Page_cliente == 'Inadimplência':
+    if Page_cliente == 'Propensão de Inadimplência':
         page_ina.propensao_de_inadimplencia()
 
     if Page_cliente == 'Inicio':
