9ccb2e75f32b3a469ebe0d1615de8d0e73e1512c
Gabriel do Nascimento <54758393+gabuxt@users.noreply.github.com>
2024-02-06 21:21:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,46 +3,22 @@ import requests
 import streamlit as st
 import json
 
-
 BASE_URL = "http://172.214.88.215"
 
 def dados_inadimplencia(body):
-    body_json = json.dumps(body)
     url = f'{BASE_URL}/predict?model=default_propensity'
-    resposta = requests.post(url, data=body_json)
-    if resposta.status_code == 200:
+    headers = {'Content-Type': 'application/json'}  # Adicione os cabeçalhos JSON
+    try:
+        resposta = requests.post(url, data=json.dumps(body), headers=headers)
+        resposta.raise_for_status()  # Lança uma exceção para erros HTTP
         return resposta.json()
-    else:
-        return "erro"
-    
+    except requests.exceptions.RequestException as e:
+        st.error(f"Erro na requisição: {e}")
+        return None
+
 def ui():
-    st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
+    # ... (seu código permanece o mesmo)
 
-    st.title('FINTECH QUANTUMFINANCE')
-    occupancy_type = st.text_input('Insira o occupancy_type:')
-    co_applicant_credit_type = st.text_input('Insira o co_applicant_credit_type:')
-    credit_Score = st.text_input('Insira o Credit_Score:')
-    lvt = st.text_input('Insira o LVT:')
-    loan_purpose = st.text_input('Insira o loan_purpose:')
-    dtir1 = st.text_input('Insira o dtir1:')
-    property_value = st.text_input('Insira o property_value:')
-    submission_of_application = st.text_input('Insira o submission_of_application:')
-    approv_in_adv = st.text_input('Insira o approv_in_adv:')
-    income = st.text_input('Insira o income:')
-
-    body = {
-        'occupancy_type': f"{occupancy_type}",
-        'co_applicant_credit_type': f"{co_applicant_credit_type}",
-        'Credit_Score': float(credit_Score),
-        'lvt': float(lvt),
-        'loan_purpose': f"{loan_purpose}",
-        'dtir1': float(dtir1),
-        'property_value': float(property_value),
-        'submission_of_application': f"{submission_of_application}",
-        'approv_in_adv': f"{approv_in_adv}",
-        'income': float(income)
-    }
-    
     if st.button('Executar'):
         retorno = dados_inadimplencia(body)
         if retorno is not None:
