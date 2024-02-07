807e03464538dee7bf4ef57708d50a13387e2f47
Gabriel do Nascimento <54758393+gabuxt@users.noreply.github.com>
2024-02-06 22:16:41 -0300

    Add files via upload

--- Pages/propensao_de_inadimplencia.py
+++ Pages/propensao_de_inadimplencia.py
@@ -24,7 +24,6 @@ def icon(emoji: str):
     )
 
 def propensao_de_inadimplencia():
-    st.set_page_config("Streamlit Components Hub", "🏦", layout="wide")
     icon("🏦")
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
 
