5e6745747f857c678663f8328306965c862dc964
Gabriel do Nascimento <54758393+gabuxt@users.noreply.github.com>
2024-02-06 21:21:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,32 @@ def dados_inadimplencia(body):
         return None
 
 def ui():
-    # ... (seu código permanece o mesmo)
+    st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
+
+    st.title('FINTECH QUANTUMFINANCE')
+    occupancy_type = st.text_input('Insira o occupancy_type:')
+    co_applicant_credit_type = st.text_input('Insira o co_applicant_credit_type:')
+    credit_Score = st.text_input('Insira o Credit_Score:')
+    lvt = st.text_input('Insira o LVT:')
+    loan_purpose = st.text_input('Insira o loan_purpose:')
+    dtir1 = st.text_input('Insira o dtir1:')
+    property_value = st.text_input('Insira o property_value:')
+    submission_of_application = st.text_input('Insira o submission_of_application:')
+    approv_in_adv = st.text_input('Insira o approv_in_adv:')
+    income = st.text_input('Insira o income:')
+
+    body = {
+        'occupancy_type': f"{occupancy_type}",
+        'co_applicant_credit_type': f"{co_applicant_credit_type}",
+        'Credit_Score': float(credit_Score),
+        'lvt': float(lvt),
+        'loan_purpose': f"{loan_purpose}",
+        'dtir1': float(dtir1),
+        'property_value': float(property_value),
+        'submission_of_application': f"{submission_of_application}",
+        'approv_in_adv': f"{approv_in_adv}",
+        'income': float(income)
+    }
 
     if st.button('Executar'):
         retorno = dados_inadimplencia(body)
