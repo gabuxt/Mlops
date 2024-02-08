efab2d7b6d5a4b4cb665188aa38805cc46fad9e4
Gabriel do Nascimento <54758393+gabuxt@users.noreply.github.com>
2024-02-08 17:13:19 -0300

    Update propensao_de_inadimplencia.py

--- Pages/propensao_de_inadimplencia.py
+++ Pages/propensao_de_inadimplencia.py
@@ -56,7 +56,4 @@ def propensao_de_inadimplencia():
 
         retorno = dados_inadimplencia(body)
         if retorno is not None:
-            if(retorno.prediction == 0):
                 st.write('Sem propensão a inadimplência')
-            else
-                st.write('Com propensão a inadimplência')
